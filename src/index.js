28094c225b43b86f29ce812b752cce15b65849ca
Kamil Sokołowski <sokolowski.k@outlook.com>
2018-01-27 14:23:08 +0100

    Add immutablejs support

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import IntlReducer from './reducer'
 import withTranslate from './withTranslate'
 import IntlProvider from './provider'
 import * as IntlActions from './actions'
+import * as immutable from './immutable'
+
+var ImmutableIntlReducer = immutable.default.reducer
+var ImmutableIntlProvider = immutable.default.provider
 
 export {
   IntlReducer,
   withTranslate,
   IntlProvider,
-  IntlActions
+  IntlActions,
+  ImmutableIntlReducer,
+  ImmutableIntlProvider
 }
